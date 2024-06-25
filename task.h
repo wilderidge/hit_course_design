1d67ea449a56ada3a38cce1f89fb5185a9cd8335
byislearner <103123473+byislearner@users.noreply.github.com>
2024-06-26 07:55:14 +0800

    任务二、三

---
+++ task.h
@@ -0,0 +1,7 @@
+#include "public/CheckMatrix.h"
+
+void matrix_check(double **A, double *B, double *C);
+int CheckEmptyAndSingletonRows(double *B, int m, int n, RowInfo **Rows, int *RowsSize ,int nThread);
+int CheckEmptyAndSingletonCols(double *C, int m, int n, ColInfo **Cols, int *ColsSize, int nThread);
+int CheckDuplicatedColumns(double **A, int m, int n, DupColInfo **Cols, int nThread);
+int CheckDuplicatedRows(double **A, int m, int n, DupRowInfo **DupRows, ColInfo **SingletonCols, int *nSingletons, int nThread);
